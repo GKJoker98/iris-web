228f63d1553c29ee273dc515bb5721f862e8f7a7
root <root@zrz-ux-vm15880.rz-sued.bayern.de>
2024-04-22 15:39:27 +0200

    fix import

--- source/app/blueprints/manage/manage_customers_routes.py
+++ source/app/blueprints/manage/manage_customers_routes.py
@@ -59,7 +59,7 @@ from app.util import page_not_found
 from app.util import response_error
 from app.util import response_success
 
-from source.app.datamgmt.client.client_db import export_contacts
+from app.datamgmt.client.client_db import export_contacts
 
 manage_customers_blueprint = Blueprint(
     'manage_customers',
